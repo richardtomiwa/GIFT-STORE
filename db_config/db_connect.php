8e7ddcbdb213f3249888d2b335130da424047d10
richardtomiwa <101923650+richardtomiwa@users.noreply.github.com>
2025-05-05 12:53:03 +0100

    Update db_connect.php

--- db_config/db_connect.php
+++ db_config/db_connect.php
@@ -6,5 +6,7 @@ if(!$conn){
     echo "connection error".mysqli_connect_error();
 }
 
-
+//create your own database with users and products table
+    //id, username, email, password for users table
+    //id, product_name, product_description, price, product_image, product_content
 ?>
